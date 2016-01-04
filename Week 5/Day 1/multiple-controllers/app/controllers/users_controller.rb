41f67c09e6d7884e01575c8c1211b91627474535
Jon Wexler <jwexman@gmail.com>
2016-01-04 18:19:12 -0500

    last example

---
+++ Week 5/Day 1/multiple-controllers/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+end
+
+def show
+    @user = User.find(params[:id])
+end
+
+def new
+    @user = User.new
+end
+
+def edit
+   @user = User.find(params[:id])
+end
+
+def destroy
+    @user = User.find(params[:id])
+ @user.destroy
+ redirect_to users_path
+end
+end
